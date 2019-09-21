01dee4ac67aba5e21c3b4e060dd5ebb3325ffac2
AntonBespalov <bespalov.anton@gmail.com>
2019-09-21 14:42:35 +0300

    актуализировал export_file_url и классы

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/1GYRZSS1efc20crbUqw3Va7lK8X6whjN3?alt=media&key=AIzaSyBfcGbKrHzoyb-5RVADBJpTdC3-8jpBXDU'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['ok', 'cracked']
 path = Path(__file__).parent
 
 app = Starlette()
